c3b0a2f0efe9748041ab8f3db37e0edc741109cb
kryukov@frtk.ru <kryukov@frtk.ru@5cb28834-c177-5972-c3b4-dced95c22bb7>
2012-09-12 20:05:18 +0000

    Makefile for unit tests

--- trunk/aestest/build.sh
+++
@@ -1,5 +0,0 @@
-CFLAGS="-I ../Putty -std=c89 -Wall -pedantic -ansi -Werror"
-gcc ./aestest.c ./puttymem.c $CFLAGS -c
-gcc ./aestest.o ./puttymem.o ../Putty/sshaes.c   $CFLAGS -o sshaes.out
-gcc ./aestest.o ./puttymem.o ../Putty/sshaesni.c $CFLAGS -o sshaesni.out
-rm aestest.o puttymem.o
