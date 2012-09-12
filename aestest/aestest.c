a0e7330532a76d0782e29a1a0067031b72b9c59e
Pavel Kryukov <pavelkryukov@users.noreply.github.com>
2012-09-12 19:51:28 +0000

    Unit tests for AES encrypt/decrypt

---
+++ aestest/aestest.c
@@ -0,0 +1,108 @@
+/**
+ * aestest
+ *
+ * Unit testing for AES cryptoalgorithm
+ *
+ * @author kryukov@frtk.ru
+ * @version 1.0
+ *
+ * For Putty AES NI project
+ * http://putty-aes-ni.googlecode.com/
+ */
+
+/*
+ * Coverage:
+ *
+ * void *aes_make_context(void);
+ * void aes_free_context(void *handle);
+ * void aes128_key(void *handle, unsigned char *key);
+ * void aes192_key(void *handle, unsigned char *key);
+ * void aes256_key(void *handle, unsigned char *key);
+ * void aes_iv(void *handle, unsigned char *iv);
+ * void aes_ssh2_encrypt_blk(void *handle, unsigned char *blk, int len);
+ * void aes_ssh2_decrypt_blk(void *handle, unsigned char *blk, int len);
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "ssh.h"
+
+typedef enum
+{
+	AES128 = 128,
+	AES192 = 192,
+	AES256 = 256
+} KeyType;
+
+typedef enum
+{
+	ENCRYPT,
+	DECRYPT
+} TestType;
+
+static void test(KeyType keytype, TestType testtype, unsigned int seed, unsigned blocklen, FILE *file)
+{
+	void *handle = aes_make_context();
+	const size_t keylen = (size_t)keytype;
+	unsigned char *key = (unsigned char*)malloc(sizeof(unsigned char) * keylen);
+	unsigned char *blk = (unsigned char*)malloc(sizeof(unsigned char) * blocklen);
+	unsigned char *iv  = (unsigned char*)malloc(sizeof(unsigned char) * keylen);
+
+	unsigned i;
+
+	srand(seed);
+
+	for (i = 0; i < blocklen; ++i)
+		blk[i] = rand();
+
+	for (i = 0; i < keylen; ++i)
+		key[i] = rand();
+
+	for (i = 0; i < keylen; ++i)
+		iv[i] = rand();
+
+	switch (keytype)
+	{
+	case AES128:
+		aes128_key(handle, key);
+		break;
+	case AES192:
+		aes192_key(handle, key);
+		break;
+	case AES256:
+		aes256_key(handle, key);
+		break;
+	}
+	
+	aes_iv(handle, key);
+
+	switch (testtype)
+	{
+	case ENCRYPT:
+		aes_ssh2_encrypt_blk(handle, blk, blocklen);
+		break;
+	case DECRYPT:
+		aes_ssh2_decrypt_blk(handle, blk, blocklen);
+		break;
+	}
+	
+	for (i = 0; i < blocklen; ++i)
+		fprintf(file,"%c", blk[i]);
+		
+	fprintf(file, "\n\n");
+	
+	aes_free_context(handle);
+	free(key), free(blk), free(iv);
+}
+
+int main()
+{
+	FILE *fp = fopen("output.txt", "w");
+	
+	test(AES128, ENCRYPT, 1050, 256, fp);
+	
+	fclose(fp);
+	
+	return 0;
+}
