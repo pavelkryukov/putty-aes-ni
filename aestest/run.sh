5c358660549789eab7cdd58fd1860f43cc008ab4
Pavel Kryukov <pavelkryukov@users.noreply.github.com>
2012-09-13 16:25:13 +0000

    [UnitTests] Script for easy test run

---
+++ aestest/run.sh
@@ -0,0 +1,37 @@
+if [ -f "sshaesni" ];
+then
+    echo "sshaeni is found"
+    if [ -f "sshaes" ]; 
+    then
+        echo "sshaes is found"
+    else
+        echo "sshaes is not found. Running make all"
+        make all
+    fi
+else
+    echo "sshaesni.out is not found. Running make all"
+    make all
+fi
+
+if [ -f "original.txt" ];
+then
+    echo "Original trace is found, it will be reused"
+else
+    echo "No original trace file. It will be generated..."
+    ./sshaes
+    mv output.txt original.txt
+fi
+
+echo "Generating local trace..."
+./sshaesni original.txt
+
+echo "MD5 check..."
+original=$(md5 original.txt)
+changed=$(md5 output.txt)
+
+if [ "$a" = "$b" ]
+then 
+    echo "Tests passed!"
+else
+    vim -d original.txt output.txt
+fi
