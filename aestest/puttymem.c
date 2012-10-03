92c19d03faf177bcc65f2bebecc694bc219caaa9
Pavel Kryukov <pavelkryukov@users.noreply.github.com>
2012-10-03 20:36:54 +0000

    [AES-NI] Fix for memory alignment

--- aestest/puttymem.c
+++ aestest/puttymem.c
@@ -15,6 +15,34 @@
 #include <limits.h>
 #include <string.h>
 
+void * aligned_malloc(size_t size, int align) {
+	void *ptr, *mem;
+	if (align < 0) {
+		return NULL;
+	}
+
+    mem = malloc(size + align - 1 + sizeof(void*));
+	
+	if (mem != NULL) {
+		ptr = (void*) (((unsigned int)mem + sizeof(void*) + align -1) & ~(align-1));
+		*((void**)((unsigned int)ptr - sizeof(void*))) = mem;
+		/* Return the address of aligned memory */
+		return ptr; 
+	}
+	return NULL;
+}
+
+void aligned_free(void *p) {
+	/* Get the address of the memory, stored at the
+	 * start of our total memory area. Alternatively,
+	 * you can use void *ptr = *((void **)p-1) instead 
+	 * of the one below.
+	 */
+	void *ptr = *((void**)((unsigned int)p - sizeof(void*)));
+	free(ptr);
+	return;
+}
+
 void *safemalloc(size_t n, size_t size)
 {
     void *p;
@@ -25,7 +53,7 @@ void *safemalloc(size_t n, size_t size)
         size *= n;
         if (size == 0)
             size = 1;
-        p = malloc(size);
+        p = aligned_malloc(size, 16);
     }
 
     return p;
@@ -34,7 +62,7 @@ void *safemalloc(size_t n, size_t size)
 void safefree(void *ptr)
 {
     if (ptr)
-        free(ptr);
+        aligned_free(ptr);
 }
 
 void smemclr(void *b, size_t n) {
