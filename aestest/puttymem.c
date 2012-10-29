7b1d6a85d99f150f0fa1b9e8a3fdc4460d79a3e8
Pavel Kryukov <pavelkryukov@users.noreply.github.com>
2012-10-29 14:01:25 +0000

    [AES-NI] Local, cross-platform and simple 16-bytes alignment of Key Expansion

--- aestest/puttymem.c
+++ aestest/puttymem.c
@@ -15,28 +15,6 @@
 #include <limits.h>
 #include <string.h>
 
-void * aligned_malloc(size_t size, int align)
-{
-    void* mem;
-    if (align < 0) {
-        return NULL;
-    }
-
-    mem = malloc(size + align - 1 + sizeof(void*));
-
-    if (mem != NULL) {
-        void* ptr = (void*)(((size_t)mem + sizeof(void*) + align - 1) & ~(align - 1));
-        *((void**)((size_t)ptr - sizeof(void*))) = mem; /* Store original ptr */
-        return ptr; 
-    }
-    return NULL;
-}
-
-void aligned_free(void *p)
-{
-    free(*((void**)((size_t)p - sizeof(void*))));
-}
-
 void *safemalloc(size_t n, size_t size)
 {
     void *p;
@@ -47,7 +25,7 @@ void *safemalloc(size_t n, size_t size)
         size *= n;
         if (size == 0)
             size = 1;
-        p = aligned_malloc(size, 16);
+        p = malloc(size);
     }
 
     return p;
@@ -56,7 +34,7 @@ void *safemalloc(size_t n, size_t size)
 void safefree(void *ptr)
 {
     if (ptr)
-        aligned_free(ptr);
+        free(ptr);
 }
 
 void smemclr(void *b, size_t n) {
