f998fd740416c964e04715cf00b8d9a43d585286
Pavel Kryukov <pavelkryukov@users.noreply.github.com>
2012-11-02 14:41:27 +0000

    [AES] Merge of AES-NI and SW AES

--- generate.sh
+++ generate.sh
@@ -4,15 +4,13 @@
 # Generator of PuTTY makefiles with AES-NI
 #
 # @author kryukov@frtk.ru
-# @version 2.0
+# @version 2.3
 #
 # For Putty AES NI project
 # http://putty-aes-ni.googlecode.com/
 
 cd ./Putty/
-perl -p -i -e 's/sshaes/sshaesni/g' ./Recipe
 perl -p -i -e 's/-Wall/-Wall -maes -msse4/g' ./mkfiles.pl
 perl ./mkfiles.pl
-perl -p -i -e 's/sshaesni/sshaes/g' ./Recipe
 perl -p -i -e 's/-Wall -maes -msse4/-Wall/g' ./mkfiles.pl
 cd ..
