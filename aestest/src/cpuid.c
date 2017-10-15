2611bc61fe86234a4b50b62423a40463586367a7
Pavel I. Kryukov <kryukov@frtk.ru>
2017-10-15 19:02:00 +0300

    Check both AES and SSE4.1 extensions

--- aestest/src/cpuid.c
+++ aestest/src/cpuid.c
@@ -21,7 +21,7 @@ static int CheckCPUsupportAES()
 {
     unsigned int CPUInfo[4];
     __cpuid(1, CPUInfo[0], CPUInfo[1], CPUInfo[2], CPUInfo[3]);
-    return CPUInfo[2] & (1 << 25);
+    return (CPUInfo[2] & (1 << 25)) && (CPUInfo[2] & (1 << 19)); /* Check AES and SSE4.1 */
 }
 
 #else /* defined(__clang__) || defined(__GNUC__) */
@@ -30,7 +30,7 @@ static int CheckCPUsupportAES()
 {
     unsigned int CPUInfo[4];
     __cpuid(CPUInfo, 1);
-    return CPUInfo[2] & (1 << 25);
+    return (CPUInfo[2] & (1 << 25)) && (CPUInfo[2] & (1 << 19)); /* Check AES and SSE4.1 */
 }
 
 #endif /* defined(__clang__) || defined(__GNUC__) */
