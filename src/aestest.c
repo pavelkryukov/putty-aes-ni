dd590e05608ac62edf65a6c852548a713e01bd2a
Pavel I. Kryukov <kryukov@frtk.ru>
2018-03-28 11:34:13 +0300

    Test AES in CBC mode by encrypting/decrypting seqyence

--- src/aestest.c
+++ src/aestest.c
@@ -16,14 +16,15 @@
 #include "coverage.h"
 #include "defines.h"
 
-static void test(KeyType keytype, TestType testtype, unsigned int seed, unsigned blocklen, FILE *file, unsigned char* arena)
+static void test(KeyType keytype, TestType testtype, unsigned int seed, unsigned blocklen, unsigned blockcount, FILE *file, unsigned char* arena)
 {
+    unsigned data_size = blocklen * blockcount
     void *handle = aes_make_context();
     const size_t keylen = (size_t)keytype;
-    const size_t usedlen = 2 * keylen + blocklen;
+    const size_t usedlen = 2 * keylen + data_size;
     unsigned char * const key = arena;
     unsigned char * const blk = arena + keylen;
-    unsigned char * const iv  = arena + keylen + blocklen;
+    unsigned char * const iv  = arena + keylen + data_size;
 
     unsigned i;
 
@@ -47,19 +48,24 @@ static void test(KeyType keytype, TestType testtype, unsigned int seed, unsigned
 
     aes_iv(handle, iv);
 
-    switch (testtype)
+    unsigned char * blk_start = blk;
+    for (i = 0; i < blockcount; ++i)
     {
-    case ENCRYPT:
-        aes_ssh2_encrypt_blk(handle, blk, blocklen);
-        break;
-    case DECRYPT:
-        aes_ssh2_decrypt_blk(handle, blk, blocklen);
-        break;
-    case SDCTR:
-        break;
+        switch (testtype)
+        {
+        case ENCRYPT:
+            aes_ssh2_encrypt_blk(handle, blk_start, blocklen);
+            break;
+        case DECRYPT:
+            aes_ssh2_decrypt_blk(handle, blk_start, blocklen);
+            break;
+        case SDCTR:
+            break;
+        }
+        blk_start += blocklen;
     }
-    
-    for (i = 0; i < blocklen; ++i)
+
+    for (i = 0; i < data_size; ++i)
         fprintf(file,"%02x ", blk[i]);
 
     fprintf(file, "\n\n");
@@ -76,17 +82,22 @@ int main(int args, char** argv)
 
     size_t blocksizes[] = {64, 128, 192, 256, 1024, 2048, 65536};    
     size_t blocksizes_s = DIM(blocksizes);
-    unsigned k, b, seed, n;
 
+    size_t blockcounts[] = {1, 2, 4, 16};
+    size_t blockcounts_s = DIM(blockcounts);
+
+    unsigned k, b, seed, n, c;
+    
     n = strtoul(argv[1], NULL, 0);
     arena = (unsigned char*)malloc(1 << 20);
 
     for (seed = 2; seed < n; ++seed)
         for (b = 0; b < blocksizes_s; ++b)
-            for (k = 0; k < keytypes_s; ++k) {
-                test(keytypes[k], ENCRYPT, seed, blocksizes[b], fp, arena);
-                test(keytypes[k], DECRYPT, seed, blocksizes[b], fp, arena);
-            }
+            for (k = 0; k < keytypes_s; ++k)
+                 for (c = 0; c < blockcounts_s; ++c) {
+                    test(keytypes[k], ENCRYPT, seed, blocksizes[b], blockcounts[c], fp, arena);
+                    test(keytypes[k], DECRYPT, seed, blocksizes[b], blockcounts[c], fp, arena);
+                 }
 
     free(arena);
     fclose(fp);
