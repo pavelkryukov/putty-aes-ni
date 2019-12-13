b81d4a6fa1991050bd59d72c99069dbcbbb81760
Pavel I. Kryukov <kryukov@frtk.ru>
2019-12-13 22:05:27 +0300

    Add ARM cpuid

--- aescpuid.c
+++ aescpuid.c
@@ -12,6 +12,8 @@
 
 #include <stdio.h>
 
+#if defined(__x86_64__) || defined(__i386)
+
 #if defined(__clang__) || defined(__GNUC__)
 
 #include <cpuid.h>
@@ -33,9 +35,27 @@ static int CheckCPUsupportAES()
 
 #endif /* defined(__clang__) || defined(__GNUC__) */
 
+#else /* defined(__x86_64__) || defined(__i386) */
+    
+#include <sys/auxv.h>
+#include <asm/hwcap.h>
+
+static int CheckCPUsupportAES(void)
+{
+#if defined HWCAP_AES
+    return getauxval(AT_HWCAP) & HWCAP_AES;
+#elif defined HWCAP2_AES
+    return getauxval(AT_HWCAP2) & HWCAP2_AES;
+#else
+    return 0;
+#endif
+}
+
+#endif /* defined(__x86_64__) || defined(__i386) */
+
 int main(int argc, char ** argv)
 {
-    const int res = !CheckCPUsupportAES();
-    printf("This CPU %s AES-NI\n", res ? "does not support" : "supports");
-    return res;
+    const int does_not_support = CheckCPUsupportAES() == 0;
+    printf("This CPU %s AES-NI\n", does_not_support ? "does not support" : "supports");
+    return does_not_support;
 }
